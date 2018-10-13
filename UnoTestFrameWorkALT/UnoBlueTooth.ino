68175c21da147ed87a7c526ca4e65ee3499752c9
van-vu-mq <36938569+van-vu-mq@users.noreply.github.com>
2018-10-13 17:43:48 +1100

    Rebuilds string into array

--- UnoTestFrameWorkALT/UnoBlueTooth.ino
+++ UnoTestFrameWorkALT/UnoBlueTooth.ino
@@ -563,7 +563,24 @@ boolean receivedNewData() {
   @return
 */
 void rebuildData(String dataFromBT) {
+  // calculate array size
+  for (int i = 0; i < dataFromBT.length(); i++) {
+    if (dataFromBT.charAt(i) == lineStartMarker) {
+      storedSize++;
+    }
+  }
 
+  // change array size storage
+  storedTransmission = new String[storedSize];
+
+  // store in parts
+  for (int i = 0; i < storedSize; i++) {
+    int startMarkerPos = dataFromBT.indexOf(lineStartMarker);
+    int endMarkerPos = dataFromBT.indexOf(lineEndMarker);
+    String line = dataFromBT.substring(startMarkerPos, endMarkerPos + 1);
+    dataFromBT.remove(startMarkerPos, endMarkerPos - startMarkerPos + 1);
+    *(storedTransmission + i) = line;
+  }
 
 }
 
